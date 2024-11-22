4d10e23dde8fe8daac5366b2acffe6452864e090
Azazacreate <111774631+Azazacreate@users.noreply.github.com>
2024-11-22 19:54:16 +0300

    commit-5

---
+++ h3_quarter_1/i11_database and SQL_seminar/j3.2_hw/MS SQL.sql
@@ -0,0 +1,32 @@
+HW
+/* 1} Вам необходимо проверить влияние семейного положения 
+(family_status) на средний доход клиентов (income) 
+и запрашиваемый кредит (credit_amount) . */
+SELECT family_status, COUNT(*) as cnt, AVG(income) as incomeAvg, AVG(credit_amount) as creditAvg
+FROM Clusters
+GROUP BY family_status
+ORDER BY AVG(income), AVG(credit_amount);
+-- не-нашел разницы:существенной.
+
+
+/* 2} Сколько товаров в категории Meat/Poultry. */
+SELECT COUNT(*)
+FROM Products
+WHERE categoryid IN
+	(select categoryid
+    from Categories
+    WHERE categoryname = 'Meat/Poultry');
+-- 6
+
+
+/* 3} Какой товар (название) заказывали в сумме 
+в самом большом количестве (sum(Quantity) 
+в таблице OrderDetails) ? */
+SELECT productname
+FROM Products
+WHERE productid IN 
+    (SELECT TOP 1 productid
+    FROM OrderDetails
+    GROUP BY productid
+    ORDER by sum(quantity) DESC)
+-- Gorgonzola Telino

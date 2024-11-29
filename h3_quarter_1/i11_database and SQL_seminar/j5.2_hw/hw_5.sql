da39e706b772bd6848bf038dd8ac802b48f4c576
Azazacreate <111774631+Azazacreate@users.noreply.github.com>
2024-11-29 19:55:30 +0300

    commit-10

--- h3_quarter_1/i11_database and SQL_seminar/j5.2_hw/hw_5.sql
+++ h3_quarter_1/i11_database and SQL_seminar/j5.2_hw/hw_5.sql
@@ -4,11 +4,29 @@
 сотрудника в качестве параметра и возвращает все заказы, 
 обработанные этим сотрудником.
 Пропишите запрос, который создаст требуемую процедуру. */
+CREATE PROCEDURE GetEmployeeOrders
+    @EmployeeID INT
+AS
+BEGIN
+    SELECT *
+    FROM Orders
+    WHERE EmployeeID = @EmployeeID;
+END;
 
 /* 2} Создайте таблицу EmployeeRoles, как на уроке и 
 удалите ее.
 Напишите запрос, который удалит нужную таблицу. */
+-- Создание таблицы EmployeeRoles
+CREATE TABLE EmployeeRoles (
+    RoleID INT PRIMARY KEY,
+    RoleName NVARCHAR(50)
+);
+
+-- Удаление таблицы EmployeeRoles
+DROP TABLE EmployeeRoles;
 
 /* 3}  Удалите все заказы со статусом 'Delivered' из 
 таблицы OrderStatus, которую создавали на семинаре
 Напишите запрос, который удалит нужные строки в таблице. */
+DELETE FROM OrderStatus
+WHERE Status = 'Delivered';
